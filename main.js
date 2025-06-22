4dc1ba08f7b590320dcc8e22346055f147cf8f6b
Frede <spamfolder@posteo.de>
2025-06-22 16:43:31 +0200

    Nicer button

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
 let installPrompt = null;
 const installButton = document.querySelector("#install");
-alert("Hello world!");
 
 window.addEventListener("beforeinstallprompt", (event) => {
-  if(!installButton) {
-    alert("No install button");
-  }
   event.preventDefault();
   installPrompt = event;
-  alert("Hello again");
   installButton.removeAttribute("hidden");
 });
 
